a6430e17bb21c9a2ca1e5af5a8772c3f7aa06ec5
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-03-26 01:54:29 -0400

    project begin

--- project/bitmap_dispaly.asm
+++ project/bitmap_dispaly.asm
@@ -16,9 +16,10 @@ li $t6,131072 # area of rectangle
 li $t7,1 # incremenet by one
 li $t0,4 
 li $t9,0
-li $t2,512
+li $t2,0
 li $t5,256
-
+la $a0,50
+la $a1,100
 
 fill_yellow:
 sw $t3,0($t1) # fill square
@@ -28,27 +29,50 @@ beq $t9,$t6,reset
 j fill_yellow
 
 
-# reset
+# resets frame to beginning
 reset:
 sub $t1,$t1,$t0
 sub $t9,$t9,$t7 # decrement by one
-beq $t9,0,first_rectangle
-j reset
+beq $t9,0,rectangle_X 
+j reset 
 
-reroll:
+## PROBLEMS
+# technically an infinite loop, if X is uncommented Y may never be reached because X runs forever
+# Does not work well if both loops are uncommented!
+
+## Notes
+# This prints a vertical rectangle (needs a way to go by specifications
+
+reroll_X:
+subi $t1,$t1,512
 addi $t1,$t1,2048
 add $t9,$zero,$zero
 
 
-first_rectangle: 
-sw $t4,256($t1)
+rectangle_X:
+sw $t4,780($t1)
 addi $t1,$t1,4
 add $t9,$t9,$t7
-beq $t9,128,reroll
-j first_rectangle
+beq $t9,128,reroll_X
+j rectangle_X
+
+## Notes 
+# This prints a horizontal rectangle needs a way to work with bounds
 
 
+#reroll_Y:
+#beq $t2,50,Exit
+#addi $t2,$t2,1
+#subi $t1,$t1,2048
+#addi $t1,$t1,2048
+#add $t9,$zero,$zero
 
+#rectangle_Y:
+#sw $t4,($t1)
+#addi $t1,$t1,4
+#add $t9,$t9,$t7
+#beq $t9,512,reroll_Y
+#j rectangle_Y
 
 Exit:
 li $v0,10
