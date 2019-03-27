c761272ab13a608af4d702a48ca750c7323722e4
marinov98 <Marin.Marinov38@myhunter.cuny.edu>
2019-03-26 22:47:01 -0400

    we have a cross!

--- project/bitmap_dispaly.asm
+++ project/bitmap_dispaly.asm
@@ -5,87 +5,140 @@ n:	.word 20
 m:	.word 20
 
 .text
-drawLine: 
-la $t1,frameBuffer
+la $s1,frameBuffer
+
+# variable allocation
+la $a0,n
+la $a1,m
+
+lw $a0,0($a0)
+lw $a1,0($a1)
+
+
+
+# Making sure bounds are even
 
 # colors initialization
 li $t3,0x00FFFF00 # $t3 ← yellow  
 li $t4,0x000000FF # $t4 <- blue
 
+
+# calculations
+addi $s2,$zero,512 # s2 <- 512
+addi $s3,$zero,256 # s3 <- 256
+
+add  $s4,$zero,$a1 # s4 <- m
+sll  $s4,$s4,1     # s4 <- 2m
+add  $s5,$s4,$a0   # s5 <- 2m + n
+
+sub $s6,$s2,$s5  # s6 <- 512 - (2m + n)
+srl $s6,$s6,1    # s6 <- (512 - (2m + n))/2
+
+sub $s7,$s3,$s5 # s7 <- 256 - (2m + n)
+srl $s7,$s7,1   # s7 <- (256 - (2m + n)/2
+
+
+# Checking bounds
+slt $t8,$s3,$s5 # check 2m + n < 256
+beq  $t8,1,Exit
+
+
 # counters
 li $t6,131072 # area of rectangle
-li $t9,0
-#li $a0,m
-#li $a1,n
+li $t9,0 # t9 gets 0
 li $t2,0 # t2 gets 0
-li $t5,0 # t3 gets 0
+li $t5,0 # t5 gets 0
 
+
+add $t1,$s1,$zero # s1 has address of frame
 
 fill_yellow:
 sw $t3,0($t1) # fill square
 addi $t1,$t1,4 # increment address by 4
 addi $t9,$t9,1 # increment by 1
-beq $t9,$t6,reset
+beq $t9,$t6,reset1
 j fill_yellow
 
 
 # resets frame to beginning
-reset:
-subi $t1,$t1,4 # go to previous address
-subi $t9,$t9,1 # decrement by one
-beq $t9,0,rectangle_X 
-j reset 
-# lw $t1, frameBuffer # supposed to reset but doesnt
-
-## PROBLEMS
-# technically an infinite loop, if X is uncommented Y may never be reached because X runs forever
-# Does not work well if both loops are uncommented!
+reset1:
+add $t1,$s1,$zero # start from beginning
+add $t9,$zero,$zero # set all registers used before to 0
+j rectangle_X
 
 ## Notes
 # This prints a vertical rectangle (needs a way to go by specifications
 
+###################################################
+#### Rectangle X
 
-reroll_X:
-subi $t1,$t1,512 # subtract amount wanted
+length_bottom_X:
+sll $t9,$t9,2
+sub $t1,$t1,$t9 # subtract amount wanted
 addi $t1,$t1,2048 # go to next row
 add $t9,$zero,$zero
 addi $t2,$t2,1
-beq $t2,128,Exit # length of rectangle from bottom
-j make_rectangle
+beq $t2,$s7,reset2 # length of rectangle (move to rectangle Y when finished) (512 - (n + 2m))/2
+j make_rectangle_X
 
 
 rectangle_X:
 
-length_top:
+length_top_X:
+beq  $t5,$s7,make_rectangle_X # (512 - (n  + 2m))/2
 addi $t1,$t1,2048 # go to next row
-addi $t5,$t5,1 # increment t3 by 1 
-beq  $t5,64,make_rectangle
-j length_top
+addi $t5,$t5,1 # increment t5 by 1 
 
-make_rectangle:
+j length_top_X
+
+make_rectangle_X:
 sw $t4,780($t1)
 addi $t1,$t1,4 # increment adress
 addi $t9,$t9,1 # check how many times address was incremented
-beq $t9,128,reroll_X
-j make_rectangle
+beq $t9,$a0,length_bottom_X # n goes here
+j make_rectangle_X
+
+###################################################
+
+### Rectangle Y 
 
 ## Notes 
 # This prints a horizontal rectangle needs a way to work with bounds
 
+# resets frame to beginning again
+reset2:
+add $t1,$s1,$zero
+# set all registers used before to 0
+add $t9,$zero,$zero
+add $t5,$zero,$zero
+add $t2,$zero,$zero
+j rectangle_Y
 
-#reroll_Y:
-#beq $t2,50,Exit
-#addi $t2,$t2,1
-#subi $t1,$t1,2048
-#addi $t1,$t1,2048
-#add $t9,$zero,$zero
+length_bottom_Y:
+sll  $t9,$t9,2
+sub $t1,$t1,$t9 # subtract amount wanted
+addi $t1,$t1,2048 # go to next row
+add $t9,$zero,$zero
+addi $t2,$t2,1
+beq $t2,$a0,Exit # length of rectangle from bottom (512 - (n + 2m))/2
+j make_rectangle_Y
 
-#rectangle_Y:
-#sw $t4,($t1)
-#addi $t1,$t1,4
-#add $t9,$t9,$t7
-#beq $t9,512,reroll_Y
-#j rectangle_Y
+
+rectangle_Y:
+
+length_top_Y:
+beq  $t5,$s6,make_rectangle_Y # lenght top (512 - (n + 2m)) /2
+addi $t1,$t1,2048 # go to next row
+addi $t5,$t5,1 # increment t5 by 1 
+j length_top_Y
+
+make_rectangle_Y:
+sw $t4,780($t1)
+addi $t1,$t1,4 # increment adress
+addi $t9,$t9,1 # check how many times address was incremented
+beq $t9,$s7,length_bottom_Y # 2m + n goes here
+j make_rectangle_Y
+
 
 Exit:
 li $v0,10
---
+++ project/cross.asm
@@ -0,0 +1,150 @@
+# Name: Marin P. Marinov
+.data
+frameBuffer: .space 0x80000 # 512 wide X 256 high pixels
+n:	.word 40
+m:	.word 40
+
+.text
+la $s1,frameBuffer
+add $t1,$s1,$zero # s1 has address of frame
+
+# variable allocation
+la $a0,n
+la $a1,m
+
+lw $a0,0($a0) # a0 <- n
+lw $a1,0($a1) # a1 <- m
+
+# Making sure bounds are even
+andi $t8,$a0,1 # AND n and 1
+beq  $t8,1,fix_n
+j try_next
+
+fix_n:
+addi $a0,$a0,1
+j  try_next
+
+try_next:
+andi $t8,$a1,1
+beq $t8,1,fix_m
+j Program
+
+fix_m:
+addi $a1,$a1,1
+
+
+Program:
+# colors initialization
+li $t3,0x00FFFF00 # $t3 ← yellow  
+li $t4,0x000000FF # $t4 <- blue
+
+
+# calculations
+addi $s2,$zero,512 # s2 <- 512
+addi $s3,$zero,256 # s3 <- 256
+
+sll $t0,$a0,2 # t0 <- 4n
+
+srl $t2,$a0, 1 # t2 <- n/2
+sub $t2,$s3,$t2 # t2 <- 256 - n/2
+
+add  $s4,$zero,$a1 # s4 <- m
+sll  $s4,$s4,1     # s4 <- 2m
+add  $s5,$s4,$a0   # s5 <- 2m + n
+
+sub $s6,$s2,$s5  # s6 <- 512 - (2m + n)
+srl $s6,$s6,1    # s6 <- (512 - (2m + n))/2
+
+sub $s7,$s3,$s5 # s7 <- 256 - (2m + n)
+srl $s7,$s7,1   # s7 <- (256 - (2m + n)/2
+
+# Checking bounds
+slt $t8,$s3,$s5 # check 2m + n < 256
+beq  $t8,1,Exit
+
+
+# counters
+li $t6,131072 # area of rectangle
+li $t7,0
+
+
+fill_yellow:
+sw $t3,0($t1) # fill square
+addi $t1,$t1,4 # increment address by 4
+addi $t9,$t9,1 # increment by 1
+beq $t9,$t6,reset1
+j fill_yellow
+
+
+# resets frame to beginning
+reset1:
+add $t1,$s1,$zero # start from beginning
+add $t9,$zero,$zero # set all registers used before to 0
+add $s0,$zero,$zero # s0 <- 0
+sll $t6,$s2,2  # t6 <- 2048
+
+##### VERTICAL Rectangle
+rectangle_X:
+sll $s7,$s7, 11  # multiply (256 - (2m + n)/2 by 2^11 
+add $t1,$t1,$s7 # go to starting row
+sll $t5,$t2,2 # t5 <- 4(256 - n/2)
+add $t1,$t1,$t5 # go to starting column
+
+
+fill_in_row:
+sw $t4, 0($t1) # store blue
+beq $t7,$a0,next_row  # n length
+addi $t1,$t1,4
+addi $t7,$t7,1
+j fill_in_row
+
+next_row:
+beq $s0,$s5,reset2 # if s0 = 2m + n go make the next rectangle
+addi $s0,$s0,1
+sub  $t1,$t1,$t0
+addi  $t1,$t1,2048 # go to next row
+add $t7,$zero,$zero # t7 is reset
+j   fill_in_row
+#############
+
+## HORIZONTAL rectangle
+reset2:
+add $t1,$s1,$zero
+# set all registers used before to 0
+add $t7,$zero,$zero # t7 <- 0
+sll $s4,$s4,1 # s4 <- 4m
+add $s0,$zero,$zero # s0 <- 0
+sll $t9,$s5,2 # t9 <- 4n
+
+
+rectangle_Y:
+sll $t5,$s6,2 # t5 <- 4(512 - n/2)
+add $t1,$t1,$t5 
+sll $t9,$s4,2 # t9 <- 4m
+#sub $t1,$t1,$t9 # go to starting row 4[(256-n/2) - m]
+sll $t9,$s5,2 # t9 <- 4(2m + n)
+subi $t2,$t2,128 # s3 <- 256 n/2 -128 
+sll $t2,$t2,11
+add $t1,$t1,$t2 # move to proper row
+
+fillrow:
+sw $t4, 0($t1)
+beq $t7,$s5,nextrow # when reached 2m + n length got to next row
+addi $t1,$t1,4 # fill current column with blue
+addi $t7,$t7,1 # keep track how many times we have filled a column
+j fillrow
+
+nextrow:
+beq $s0,$a0,Exit # if done n times exit, we are done
+addi $s0,$s0,1 # check how many times we went to the next row
+sub $t1,$t1,$t9 # subtract the offset
+addi $t1,$t1,2048 # go to next row
+add $t7,$zero,$zero # t7 <- 0
+j fillrow # start filling the row 
+
+
+Exit:
+li $v0,10
+syscall
+
+
